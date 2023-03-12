88c52e6a45991a633e4704094b57bb272e38c5df
Hitesh Rupchandani <hitesh-rupchandani@users.noreply.github.com>
2023-03-12 07:00:25 +0000

    Solution of 'Number of Steps to Reduce a Number to Zero'

---
+++ easy/05_count_steps/solution.py
@@ -0,0 +1,63 @@
+"""
+    Number of Steps to Reduce a Number to Zero
+
+    Given an integer num, return the number of steps to reduce it to zero.
+
+    In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+    Example 1:
+    Input: num = 14
+    Output: 6
+    Explanation: 
+        Step 1) 14 is even; divide by 2 and obtain 7. 
+        Step 2) 7 is odd; subtract 1 and obtain 6.
+        Step 3) 6 is even; divide by 2 and obtain 3. 
+        Step 4) 3 is odd; subtract 1 and obtain 2. 
+        Step 5) 2 is even; divide by 2 and obtain 1. 
+        Step 6) 1 is odd; subtract 1 and obtain 0.
+     
+    Example 2:
+    Input: num = 8
+    Output: 4
+    Explanation: 
+        Step 1) 8 is even; divide by 2 and obtain 4. 
+        Step 2) 4 is even; divide by 2 and obtain 2. 
+        Step 3) 2 is even; divide by 2 and obtain 1. 
+        Step 4) 1 is odd; subtract 1 and obtain 0.
+    
+    Example 3:
+    Input: num = 123
+    Output: 12
+"""
+
+class Solution:
+    """
+    Solution class for Number of Steps to Reduce a Number to Zero.
+    """
+
+    def number_of_steps(self, num: int) -> int:
+        """
+            fizz_buzz
+        """
+        steps = 0
+        while num > 0:
+            if num % 2 == 0:
+                num = num // 2
+                steps += 1
+            else:
+                num = int(num - 1)
+                steps += 1
+        return steps  
+
+def main() -> None:
+    """
+        This is main method or driver method
+    """
+    
+    solution = Solution()
+    print(solution.number_of_steps(num=14))
+    print(solution.number_of_steps(num=8))
+    print(solution.number_of_steps(num=123))
+
+if __name__ == "__main__":
+    main()
